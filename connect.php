a489384ad596dd88298abe2c71fe48e9344b568a
Henry C. Yee Jr. <h.yee32@gmail.com>
2024-09-13 16:14:48 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         if (password_verify($input_password, $user['password'])) {
             
-            $_SESSION['user_id'] = $user['id'];
+            //$_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['username'];
             header("Location: dashboard.html");
             exit;
